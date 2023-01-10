5e723bb0ac268ab83ff85059c292acf071d2f5e6
sepideh-biglarizadeh <sepideh.biglarizadeh00@gmail.com>
2023-01-10 13:45:51 +0330

    os module

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-const path = require("path");
-let parsedPath = path.parse(__filename);
-console.log(parsedPath);
-
-// ? you can use path module to work with url
+const os = require("os");
+console.log(os.freemem()); //? return free memory
+console.log(os.totalmem()); //? return total memory
