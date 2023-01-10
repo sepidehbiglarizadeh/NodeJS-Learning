036a93df6bf499b0d5f4ea7235321c97e6ee3da5
sepideh-biglarizadeh <sepideh.biglarizadeh00@gmail.com>
2023-01-10 18:40:46 +0330

    using static middleware and helmet middlware

--- app.js
+++ app.js
@@ -2,35 +2,18 @@ const express = require("express"); //? returns a function
 let users = require("./users");
 const app = express(); //? returns an object
 const { body, validationResult } = require("express-validator");
+const helmet = require("helmet");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-app.use((req, res, next) => {
-  req.body.userName = "test";
-  req.user = { id: 1, name: "test1" };
-  // res.send("this response is coming from middleware1");
-  console.log("midd1");
-  next();
-});
-
-app.use((req, res, next) => {
-  console.log("midd2");
-  console.log(req.body);
-  console.log(req.user);
-  next();
-});
+app.use(express.static("public"));
+app.use(helmet());
 
 app.get("/api/users", (req, res) => {
   res.json({
     data: users,
     message: "ok",
   });
-});
-
-app.use((req, res, next) => {
-  console.log("midd3");
-  next();
 });
 
 app.get("/api/users/:id", (req, res) => {
