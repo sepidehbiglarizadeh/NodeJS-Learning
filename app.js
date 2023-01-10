7d6357a8d461b750fee40fd8b0d2ce03296ae468
sepideh-biglarizadeh <sepideh.biglarizadeh00@gmail.com>
2023-01-10 13:51:06 +0330

    fs module

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
-const os = require("os");
-console.log(os.freemem()); //? return free memory
-console.log(os.totalmem()); //? return total memory
+const fs = require("fs");
+
+//* sync
+const files = fs.readdirSync("./"); //? returns the files in the path
+console.log(files);
+
+//* Async
+const files2 = fs.readdir("./", (err, file) => {
+  console.log(err);
+  console.log(file);
+});
+console.log("Hello");
