9bedc30de990b7f9345257171bce0a39b50ade3b
sepideh-biglarizadeh <sepideh.biglarizadeh00@gmail.com>
2023-01-10 15:16:27 +0330

    create an api that get a user

--- app.js
+++ app.js
@@ -2,9 +2,16 @@ const express = require("express"); //? returns a function
 const users = require("./users");
 const app = express(); //? returns an object
 
-app.get("/api/users", (req, res) => {
+app.get("/api/users/:id", (req, res) => {
+  const user = users.find((u) => u.id === parseInt(req.params.id));
+  if (!user) {
+    return res.status(404).json({
+      data: null,
+      message: "user with given id was not found",
+    });
+  }
   res.json({
-    data: users,
+    data: user,
     message: "ok",
   });
 });
