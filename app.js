a3cfd7cb87de786d65db597d0adf72fc5a0ddfd0
sepideh-biglarizadeh <sepideh.biglarizadeh00@gmail.com>
2023-01-14 16:43:41 +0330

    connecting the project to the database

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const debug = require("debug")("app:main");
 const dbdebug = require("debug")("app:db");
 const userRouter = require("./routes/users");
 const homeRouter = require("./routes/home");
+const mongoose = require("mongoose");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -16,6 +17,11 @@ app.use(helmet());
 
 app.set("view engine", "ejs");
 app.set("views", "./views");
+
+mongoose
+  .connect("mongodb://127.0.0.1:27017/helloexpress")
+  .then(() => console.log("connected to mongodb"))
+  .catch(() => console.log("could not connect to mogodb"));
 
 if (app.get("env") === "development") {
   debug("morgan is active");
--- routes/users.js
+++ routes/users.js
@@ -1,18 +1,18 @@
 const express = require("express");
 const router = express.Router();
 const { body, validationResult } = require("express-validator");
-let users = require("../users");
+const User = require("../models/user");
 
-
-router.get("/", (req, res) => {
+router.get("/", async (req, res) => {
+  const users = await User.find();
   res.json({
     data: users,
     message: "ok",
   });
 });
 
-router.get("/:id", (req, res) => {
-  const user = users.find((u) => u.id === parseInt(req.params.id));
+router.get("/:id", async (req, res) => {
+  const user = await User.findById(req.params.id);
   if (!user) {
     return res.status(404).json({
       data: null,
@@ -32,7 +32,7 @@ router.post(
     body("first_name", "first name cant be empty").notEmpty(),
     body("last_name", "last name cant be empty").notEmpty(),
   ],
-  (req, res) => {
+  async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({
@@ -41,9 +41,14 @@ router.post(
         message: "validation error",
       });
     }
-    users.push({ id: users.length + 1, ...req.body });
+    let newUser = new User({
+      first_name: req.body.first_name,
+      last_name: req.body.last_name,
+      email: req.body.email,
+    });
+    newUser = await newUser.save();
     res.json({
-      data: users,
+      data: newUser,
       message: "ok",
     });
   }
@@ -56,14 +61,7 @@ router.put(
     body("first_name", "first name cant be empty").notEmpty(),
     body("last_name", "last name cant be empty").notEmpty(),
   ],
-  (req, res) => {
-    const user = users.find((u) => u.id === parseInt(req.params.id));
-    if (!user) {
-      return res.status(404).json({
-        data: null,
-        message: "user with given id was not found",
-      });
-    }
+  async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({
@@ -72,33 +70,40 @@ router.put(
         message: "validation error",
       });
     }
-    users = users.map((user) => {
-      if (user.id === parseInt(req.params.id)) {
-        return { ...user, ...req.body };
-      }
-      return user;
-    });
+    const user = await User.findByIdAndUpdate(
+      req.params.id,
+      {
+        first_name: req.body.first_name,
+        last_name: req.body.last_name,
+        email: req.body.email,
+      },
+      { new: true }
+    );
+    if (!user) {
+      return res.status(404).json({
+        data: null,
+        message: "user with given id was not found",
+      });
+    }
     res.json({
-      data: users,
+      data: user,
       message: "ok",
     });
   }
 );
 
-router.delete("/:id", (req, res) => {
-  const user = users.find((u) => u.id === parseInt(req.params.id));
+router.delete("/:id", async (req, res) => {
+  const user = await User.findByIdAndRemove(req.params.id);
   if (!user) {
     return res.status(404).json({
       data: null,
       message: "the user with the given id was not found",
     });
   }
-  const index = users.indexOf(user);
-  users.splice(index, 1);
   res.json({
-    data: users,
+    data: user,
     message: "ok",
   });
 });
 
-module.exports= router;
+module.exports = router;
